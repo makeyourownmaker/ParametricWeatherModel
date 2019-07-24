4904e109983278ee1d002b6a7b5c64e9945fd5cb
Derek Wilson <derekw@derekw.org>
2019-07-24 14:36:34 +0100

    Added more command line options and included stub functions for additional radiative processes and heat fluxes

--- README.md
+++ README.md
@@ -1,14 +1,134 @@
 
 # ParametricWeatherModel
 
-ParametricWeatherModel is a python script for forecasting surface temperature based
-primarily on latitude, longitude, day of year and hour of day.
+ParametricWeatherModel is a repository for forecasting surface temperature 
+based primarily on latitude, longitude, day of year and hour of day.
 
 
 ## Development Stage
 
-It is under active but early development and contains many limitation, hardcoded
-parameters and probably errors.  If it breaks you can keep the pieces.
+The repository is under active but early development and contains many 
+limitations, hardcoded parameters and probably errors.  If it breaks you can 
+keep the pieces.
+
+
+## Usage
+
+Predict surface temperature at specified latitude, longitude, day of year, 
+ground temperature and initial surface temperature:
+```
+python parametric_scheme.py -la 47.6928 -lo -122.3038 -da 229 -gt 54 -st 72
+
+# Similarly with longer options
+python parametric_scheme.py --latitude 47.6928 --longitude -122.3038 --day_of_year 229 --ground_temp 54 --surface_temp 72
+
+# To list command line options
+python parametric_scheme.py -h
+```
+
+
+## Installation
+
+Requires a recent version of python.
+
+The following should work on any unix-ish environment:
+```
+wget https://raw.githubusercontent.com/makeyourownmaker/ParametricWeatherModel/master/parametric_scheme.py
+python parametric_scheme.py -h
+```
+
+
+## Details
+
+This repository contains code for a simple parameterization scheme to 
+forecast the air temperature at the surface at a point over the next hour.
+It is based on chapter 2 from the book 
+[Parameterization Schemes: Keys to Understanding Numerical Weather Prediction Models](https://www.cambridge.org/core/books/parameterization-schemes/C7C8EC8901957314433BE7C8BC36F16D#fndtn-information) 
+by [David J. Stensrud](http://www.met.psu.edu/people/djs78).
+
+According to [wikipedia](https://en.wikipedia.org/wiki/Parametrization_(atmospheric_modeling)):
+A parameterization scheme "in a weather or climate model in the context of 
+numerical weather prediction is a method of replacing processes that are too 
+small-scale or complex to be physically represented in the model by a 
+simplified process."
+
+The python code models the following radiative processes:
+  * incoming solar radiation
+
+Radiative processes to add:
+  * upwelling longwave radiation from the surface
+  * downwelling longwave radiation from the atmosphere
+  * sensible heat flux (heat transfer per unit area from the ground to the atmosphere)
+  * latent heat flux (rate of moisture transfer per unit area from the ground surface to the atmosphere)
+  * ground heat flux (heat transfer from the ground surface into the deeper soil levels)
+
+Included parameters:
+  Required:
+    * Latitude
+    * Longitude (plus for east, minus for west)
+    * Day of year (1 to 365)
+    * Initial surface air temperature (Fahrenheit)
+    * Ground reservoir temperature (Fahrenheit)
+  Optional:
+    * Hour of day (0 to 24) default=12
+    * Cloud fraction (0 to 1) default=0
+    * Albedo (0 to 1) default=0.3
+    * UTC offset (-12 to 12) default=0
+    * Atmospheric transmissivity default=0.8
+    * Day of solstice default=173
+    * Forecase period in seconds default=3600
+    * Help option
+    
+Parameters to add:
+  Required:
+    * Initial surface air temperature (Celsius)
+    * Ground reservoir temperature (Celsius)
+  Optional:
+    * Precipitable water
+    * Bowen ratio
+    * Atmospheric emissivity 
+    * Verbose option
+
+
+### Limitations
+
+  * Earth's elliptical orbit is ignored
+  * Some variables are treated as constants e.g. transmissivity
+  * Hardcoded parameters include: thermal diffusivity of air, soil heat capacity and solar radiation
+
+
+## Roadmap
+
+* Validate solar radiation calculations
+* Add additional heat flux terms
+  * upwelling longwave radiation from the surface
+  * downwelling longwave radiation from the atmosphere
+  * sensible heat flux (heat transfer per unit area from the ground to the atmosphere)
+  * latent heat flux (rate of moisture transfer per unit area from the ground surface to the atmosphere)
+  * ground heat flux (heat transfer from the ground surface into the deeper soil levels)
+* Add more command line options
+  * Albedo, transmissivity, emissivity ...
+* Improve documentation
+  * Expand the details section above
+    * Explain default values used and/or
+    * Include details of the constants used
+  * Possibly add some illustrative plots
+  * Add more usage examples
+    * Illustrate the most important command line options
+* Add unit tests
+
+
+## Contributing
+
+Pull requests are welcome.  For major changes, please open an issue first to discuss what you would like to change.
+
+
+## See also
+
+* [Digging into a "simple" weather model](http://lukemweather.blogspot.com/2011/08/digging-into-simple-weather-model.html) 
+  by [Luke Madaus](http://midlatitude.com/lukemadaus/)
+* [Parameterization Schemes: Keys to Understanding Numerical Weather Prediction Models](https://www.cambridge.org/core/books/parameterization-schemes/C7C8EC8901957314433BE7C8BC36F16D#fndtn-information) 
+  by [David J. Stensrud](http://www.met.psu.edu/people/djs78)
 
 
 ## License
--- parametric_scheme.py
+++ parametric_scheme.py
@@ -2,17 +2,11 @@
 import math
 import argparse
 
-# "Constants"
-k     = 11     # J m^-2 K^-1 s^-1 - Thermal diffusivity of the air??
-c_g   = 1.4 * 10**5 # J m^-2 K^-1 - Soil heat capacity
-S     = 1368   # W m^-2 - Solar irradiance
-d_bar = 1.50 * 10**11 # m - Mean distance from sun to the Earth
-d_y   = 365.25 # - days in year
-d_s   = 173    # - solstice day
-
 
 def f_to_k(f):
-    '''Convert Fahrenheit to Kelvin'''
+    '''
+    Convert Fahrenheit to Kelvin
+    '''
 
     k = (f + 459.67) * 5/9
 
@@ -20,7 +14,9 @@ def f_to_k(f):
 
 
 def k_to_f(k):
-    '''Convert Kelvin to Fahrenheit'''
+    '''
+    Convert Kelvin to Fahrenheit
+    '''
 
     f = (k - 273.15) * 9/5 + 32
 
@@ -28,7 +24,9 @@ def k_to_f(k):
 
 
 def c_to_k(c):
-    '''Convert Celsius to Kelvin'''
+    '''
+    Convert Celsius to Kelvin
+    '''
 
     k = c + 273.15
 
@@ -36,15 +34,67 @@ def c_to_k(c):
 
 
 def k_to_c(k):
-    '''Convert Kelvin to Celsius'''
+    '''
+    Convert Kelvin to Celsius
+    '''
 
     c = k - 273.15
 
     return c
 
 
+def downwelling_rad(args):
+    '''
+    Calculate downwelling longwave radiation
+    '''
+
+    Q_Ld = 0
+
+    print "Q_Ld:\t", Q_Ld
+
+    return Q_Ld
+
+
+def upwelling_rad(args):
+    '''
+    Calculate upwelling longwave radiation
+    '''
+
+    Q_Lu = 0
+
+    print "Q_Lu:\t", Q_Lu
+
+    return Q_Lu
+
+
+def sensible_heat_flux(args):
+    '''
+    Calculate sensible heat flux
+    '''
+
+    Q_H = 0
+
+    print "Q_H:\t", Q_H
+
+    return Q_H
+
+
+def latent_heat_flux(args):
+    '''
+    Calculate latent heat flux
+    '''
+
+    Q_E = 0
+
+    print "Q_E:\t", Q_E
+
+    return Q_E
+
+
 def local_hour(h_utc, lon):
-    '''Calculate local hour of the sun'''
+    '''
+    Calculate local hour of the sun
+    '''
 
     h = (h_utc - 12) * math.pi / 12 + lon * math.pi / 180
     print "h:\t", h
@@ -52,20 +102,33 @@ def local_hour(h_utc, lon):
     return h
 
 
-def declination(doy):
-    '''Calculate declination angle'''
+def declination(args):
+    '''
+    Calculate declination angle
+    '''
 
-    delta = 23.45 * math.cos(2*math.pi*(doy - d_s)/d_y)
+    # "Constants"
+    d_y = 365.25
+
+    d_s = args.day_of_solstice
+    doy = args.day_of_year
+
+    delta = 23.45 * math.cos(2 * math.pi * (doy - d_s) / d_y)
     print "delta:\t", delta
 
     return delta
 
 
-def zenith(doy, h_utc, lat, lon):
-    '''Calculate zenith angle'''
+def zenith(args):
+    '''
+    Calculate zenith angle
+    '''
 
-    h   = local_hour(h_utc, lon)
-    dec = declination(doy)
+    lat   = args.latitude
+    lon   = args.longitude
+    h_utc = t_to_utc(args)
+    h     = local_hour(h_utc, lon)
+    dec   = declination(args)
 
     zenith = math.sin(lat) * math.sin(dec) + math.cos(lat) * math.cos(dec) * math.cos(h)
     print "zenith:\t", zenith
@@ -73,33 +136,36 @@ def zenith(doy, h_utc, lat, lon):
     return zenith
 
 
-def t_to_utc(t):
-    '''Convert hour to UTC'''
+def t_to_utc(args):
+    '''
+    Convert hour to UTC
+    '''
 
-    # NOTE: Hardcoded for Seattle, WA
-    utc = t + 7
+    utc = args.hour + args.utc_offset
 
     return utc
 
 
 def solar_rad(args):
-    '''Calculate incoming solar radiation at latitude and longitude'''
+    '''
+    Calculate incoming solar radiation at latitude and longitude
+    '''
 
-    h_utc = t_to_utc(16)
-    doy   = 229
-    zen   = zenith(doy, h_utc, float(args.latitude), float(args.longitude))
-    #zen   = - zenith(doy, h_utc, float(args.latitude), float(args.longitude))
-    #print zen
+    # "Constants"
+    S     = 1368          # W m^-2 - Solar irradiance
+    d_bar = 1.50 * 10**11 # m      - Mean distance from sun to Earth
 
-    a     = 0.2 # albedo
-    tau_s = 0.8 # atmospheric transmissivity
+    tau_s = args.transmissivity # Atmospheric transmissivity
+    a     = args.albedo
+
+    #zen = zenith(args)
+    zen  = - zenith(args)
 
     if zen < 0:
         Q_S = 0
     else:
-        # Note: Ignoring elliptical orbit
-        #Q_S = S * d_bar**2 * (1 - a) * zen * tau_s
-        Q_S  = S * (1.5)**2 * (1 - a) * zen * tau_s
+        # NOTE: Ignoring elliptical orbit for now
+        Q_S  = S * 1.5**2 * (1 - a) * zen * tau_s
 
     print "Q_S:\t", Q_S
 
@@ -107,32 +173,64 @@ def solar_rad(args):
 
 
 def main(args):
-    '''Calculate surface temperature at latitude and longitude'''
+    '''
+    Calculate surface temperature at latitude and longitude
+    '''
 
-    Q_S = solar_rad(args) # Incoming solar radiation
-    Q_Ld = 0              # Downwelling longwave radiation
-    Q_Lu = 0              # Upwelling longwave radiation
-    Q_H  = 0              # Sensible heat flux
-    Q_E  = 0              # Latent heat flux
-    T_r  = f_to_k(54)     # K reservoir temperature
-    T_g_init = f_to_k(72) # K initial ground temperature
+    # "Constants"
+    c_g = 1.4 * 10**5 # J m^-2 K^-1      - Soil heat capacity
+    K   = 11          # J m^-2 K^-1 s^-1 - Thermal diffusivity of the air
 
-    T_g = (Q_S + Q_Ld - Q_Lu - Q_H - Q_E - k*(T_g_init - T_r)) 
-    #T_g = (Q_S + Q_Ld - Q_Lu - Q_H - Q_E - k*(T_g_init - T_r)) / c_g
-    # last term equivalent to Q_G the ground heat flux
+    Q_S  = solar_rad(args)               # Incoming solar radiation
+    Q_Ld = downwelling_rad(args)         # Downwelling longwave radiation
+    Q_Lu = upwelling_rad(args)           # Upwelling longwave radiation
+    Q_H  = sensible_heat_flux(args)      # Sensible heat flux
+    Q_E  = latent_heat_flux(args)        # Latent heat flux
+    T_r  = f_to_k(args.ground_temp)      # K - Reservoir temperature
+    T_g_init = f_to_k(args.surface_temp) # K - Initial ground temperature
 
-    print "T_g:\t", T_g
+    d_t   = args.forecast_period
+    d_T_g = (Q_S + Q_Ld - Q_Lu - Q_H - Q_E - K*(T_g_init - T_r)) * d_t / c_g
+    # last term approximates Q_G the ground heat flux
+
+    T_g = k_to_f(T_g_init + d_T_g)
+
+    print "d_T_g:\t", d_T_g, "K"
+    print "T_g:\t", T_g, "F"
+
     return 0
 
+
+#Parameters to add:
+#  Required:
+#    * Initial surface air temperature (Celsius)
+#    * Ground reservoir temperature (Celsius)
+#  Optional:
+#    * Precipitable water
+#    * Bowen ratio
+#    * Atmospheric emissivity 
+#    * Verbose option
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = 
             'Calculate surface temperature at latitude and longitude')
     optional = parser._action_groups.pop()
     required = parser.add_argument_group('required arguments')
-    required.add_argument('-la', '--latitude',  help='Specify latitude', required=True)
-    required.add_argument('-lo', '--longitude', help='Specify longitude', required=True)
-    optional.add_argument('-ho', '--hour', help='Specify hour of day', default=12)
+    required.add_argument('-la', '--latitude',     help='Specify latitude', required=True, type=float)
+    required.add_argument('-lo', '--longitude',    help='Specify longitude (plus for east, minus for west)', required=True, type=float)
+    required.add_argument('-da', '--day_of_year',  help='Specify Julian day of year (1 to 365)', required=True, type=int)
+    required.add_argument('-gt', '--ground_temp',  help='Specify ground reservoir temperature (Fahrenheit only)', type=float)
+    required.add_argument('-st', '--surface_temp', help='Specify initial surface air temperature (Fahrenheit only)', type=float)
+    
+    #optional.add_argument('-v',  '--verbose',         help='Print additional information')
+    optional.add_argument('-ho', '--hour',            help='Specify hour of day (0 to 24) default=12', default=12, type=int)
+    optional.add_argument('-al', '--albedo',          help='Specify albedo (0 to 1) default=0.3', default=0.3, type=float)
+    optional.add_argument('-cf', '--cloud_fraction',  help='Specify cloud fraction (0 to 1) default=0', default=0, type=float)
+    optional.add_argument('-ds', '--day_of_solstice', help='Specify day of solstice (172 or 173) default=173', default=173, type=int)
+    optional.add_argument('-uo', '--utc_offset',      help='Specify UTC offset (-12 to 12) default=0', default=0, type=int)
+    optional.add_argument('-fp', '--forecast_period', help='Specify forecast period in seconds default=3600', default=3600, type=int)
+    optional.add_argument('-tr', '--transmissivity',  help='Specify atmospheric transmissivity default=0.8', default=0.8, type=float)
+
     parser._action_groups.append(optional)
     args = parser.parse_args()
 
